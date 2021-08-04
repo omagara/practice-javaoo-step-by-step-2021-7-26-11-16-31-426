b95d8f86a54a24c0c90713c82448f60e979a6b82
omagara <omagara@oocl.com>
2021-08-04 23:25:13 +0800

    [Practice09][Ramon]feat:implement should_teacher_have_name_and_age_and_classes

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,19 @@
 package practice09;
 
-public class Teacher {
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> linkedList = new LinkedList<Klass>();
+
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> linkedList) {
+        super(id, name, age);
+        this.linkedList=linkedList;
+    }
+
+    public LinkedList<Klass> getClasses(){
+        return this.linkedList=linkedList;
+    }
 }
