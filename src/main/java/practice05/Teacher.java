9651da65e88a4debdc4bdf75181d421d37f8f14d
omagara <omagara@oocl.com>
2021-08-04 17:11:00 +0800

    [Practice05][Ramon]feat:implement should_teacher_have_name_and_age_and_class

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,14 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
 }
