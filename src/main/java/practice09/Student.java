fef00b3abce8427a3805ea64461985d10c738c21
omagara <omagara@oocl.com>
2021-08-04 23:17:36 +0800

    [Practice09][Ramon]feat:implement should_student_introduce_itself_as_class_leader

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -10,7 +10,10 @@ public class Student extends Person{
     public Klass getKlass() {
         return klass;
     }
-    public String introduce() {
-        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am at " + klass.getDisplayName() + ".";
+    public String introduce(){
+        if (klass.getLeader()==null){
+            return "My name is "+ getName() + ". I am "+getAge()+" years old. I am a Student. I am at " + klass.getDisplayName() +".";
+        }
+        return "My name is "+ getName() + ". I am "+getAge()+" years old. I am a Student. I am Leader of " + klass.getDisplayName() +".";
     }
 }
