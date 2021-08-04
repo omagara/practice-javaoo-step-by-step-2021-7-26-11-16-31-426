4c14bb79dbf95a8f41a2b476b5af307efb3dbd83
omagara <omagara@oocl.com>
2021-08-04 17:20:48 +0800

    [Practice05][Ramon]feat:implement should_teacher_introduce_itself_with_no_class_teaching

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -7,11 +7,18 @@ public class Teacher extends Person{
         super(name, age);
         this.klass = klass;
     }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
     public int getKlass() {
         return klass;
     }
 
     public String introduce(){
-        return "My name is "+ getName() + ". I am "+getAge()+" years old. I am a Teacher. I teach Class " + klass +".";
+        if (klass==0){
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
+        }else {
+            return "My name is "+ getName() + ". I am "+getAge()+" years old. I am a Teacher. I teach Class " + klass +".";
+        }
     }
 }
--- src/test/java/practice05/Practice05Test.java
+++ src/test/java/practice05/Practice05Test.java
@@ -46,10 +46,10 @@ public class Practice05Test {
         Teacher tom = new Teacher("Tom", 21, 2);
         assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.");
     }
-//
-//    @Test
-//    public void should_teacher_introduce_itself_with_no_class_teaching() throws Exception {
-//        Teacher tom = new Teacher("Tom", 21);
-//        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.");
-//    }
+
+    @Test
+    public void should_teacher_introduce_itself_with_no_class_teaching() throws Exception {
+        Teacher tom = new Teacher("Tom", 21);
+        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.");
+    }
 }
