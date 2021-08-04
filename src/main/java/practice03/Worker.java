c152c42757de4df9bca5806f370a3a99b6cbff53
omagara <omagara@oocl.com>
2021-08-04 16:30:59 +0800

    [Practice03][Ramon]feat:implement should_worker_introduce_itself_with_no_field

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -4,4 +4,8 @@ public class Worker extends Person{
     public Worker(String name, int age) {
         super(name, age);
     }
+
+    public String introduce(){
+        return "I am a Worker. I have a job.";
+    }
 }
