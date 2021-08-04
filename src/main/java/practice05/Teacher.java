a10051b8c100da41b0a432a0fdef1b8a37aa7692
omagara <omagara@oocl.com>
2021-08-04 17:14:18 +0800

    [Practice05][Ramon]feat:implement should_teacher_introduce_itself_with_which_class_it_teaches

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -3,12 +3,15 @@ package practice05;
 public class Teacher extends Person{
     private int klass;
 
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
     public int getKlass() {
         return klass;
     }
 
-    public Teacher(String name, int age, int klass) {
-        super(name, age);
-        this.klass = klass;
+    public String introduce(){
+        return "My name is "+ getName() + ". I am "+getAge()+" years old. I am a Teacher. I teach Class " + klass +".";
     }
 }
