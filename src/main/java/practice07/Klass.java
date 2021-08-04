942c37f201d1c32cdca5b545af88435ca3c67309
omagara <omagara@oocl.com>
2021-08-04 20:19:08 +0800

    [Practice07][Ramon]feat:implement should_person_have_id_name_and_age

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,9 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
 }
--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,4 +1,25 @@
 package practice07;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
