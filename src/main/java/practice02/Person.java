dd3587fd4e37c8ced8d10acbe35f2f54a3ce95ae
omagara <omagara@oocl.com>
2021-08-04 16:16:28 +0800

    [Practice02][Ramon]feat:implement should_person_have_an_introduce_method_which_introduce_person_with_name_and_age

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -15,6 +15,10 @@ public class Person {
     public int getAge() {
         return age;
     }
+    
+    public String introduce(){
+        return "My name is " + name +". I am " + age + " years old.";
+    }
 
 
 }
