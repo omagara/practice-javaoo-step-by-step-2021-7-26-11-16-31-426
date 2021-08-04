0cd1043f6688e9ee602c7b6945f5be131ba9ccb0
omagara <omagara@oocl.com>
2021-08-04 19:56:05 +0800

    [Practice06][Ramon]feat:implement should_teacher_introduce_itself_with_no_class_teaching

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,17 +1,26 @@
 package practice06;
 
-public class Teacher extends Person{
+public class Teacher extends Person {
     private Klass klass;
+
     public Teacher(String name, int age, Klass klass) {
         super(name, age);
         this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
     }
 
     public Klass getKlass() {
         return klass;
     }
 
-    public String introduce(){
-        return "My name is "+ getName() + ". I am "+getAge()+" years old. I am a Teacher. I teach " + klass.getDisplayName() +".";
+    public String introduce() {
+        if (klass != null) {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach " + klass.getDisplayName() + ".";
+        }else {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
+        }
     }
 }
