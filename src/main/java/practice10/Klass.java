39e16315b89270e7b8d877993488974064f47822
omagara <omagara@oocl.com>
2021-08-05 08:34:39 +0800

    [Practice10][Ramon]feat:implement should_class_have_a_number

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -3,6 +3,10 @@ package practice10;
 public class Klass {
     private int number;
 
+    public int getNumber() {
+        return number;
+    }
+
     public Klass(int number) {
         this.number = number;
     }
