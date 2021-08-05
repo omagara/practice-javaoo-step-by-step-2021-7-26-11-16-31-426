76ba24334213a90d6c8952dea8b3de46c4964888
omagara <omagara@oocl.com>
2021-08-05 08:35:55 +0800

    [Practice10][Ramon]feat:implement should_class_get_display_name

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -2,12 +2,17 @@ package practice10;
 
 public class Klass {
     private int number;
-
+    private String displayName;
     public int getNumber() {
         return number;
     }
 
     public Klass(int number) {
         this.number = number;
+    }
+
+    public String getDisplayName() {
+        displayName = "Class " + number;
+        return displayName;
     }
 }
