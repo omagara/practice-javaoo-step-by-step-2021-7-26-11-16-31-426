d741d2cef38ef5f2234648a6599f02b59ea57116
omagara <omagara@oocl.com>
2021-08-05 08:33:01 +0800

    [Practice10][Ramon]feat:implement should_person_with_same_id_be_same_one

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,5 +1,7 @@
 package practice10;
 
+import java.util.Objects;
+
 public class Person {
     private int id;
     private String name;
@@ -18,4 +20,13 @@ public class Person {
         this.name = name;
         this.age = age;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && age == person.age && Objects.equals(name, person.name);
+    }
+
 }
