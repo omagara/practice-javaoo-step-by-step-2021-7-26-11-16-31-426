24cdca89026fbf6d2b9f32a3f955abbde0395cc5
omagara <omagara@oocl.com>
2021-08-06 08:32:20 +0800

    [Practice09][Ramon]refactor: added final to the Klass klass

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,7 +1,8 @@
 package practice09;
 
 public class Student extends Person{
-    private Klass klass;
+    private final Klass klass;
+
     public Student(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
@@ -10,6 +11,7 @@ public class Student extends Person{
     public Klass getKlass() {
         return klass;
     }
+
     public String introduce(){
         if (klass.getLeader()==null){
             return "My name is "+ getName() + ". I am "+getAge()+" years old. I am a Student. I am at " + klass.getDisplayName() +".";
